1a563aaf59f8a968b82bf3d96334c7db1631086d
Damian Tocev <damian.tocev@gmail.com>
2023-01-25 23:37:20 +0200

    Add project files.

---
+++ ConsoleInputSample/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleInputSample
+{
+    public class ProgramTest
+    {
+        public static void Main()
+        {
+            var nums = Console.ReadLine().Split().Select(int.Parse);
+            Console.WriteLine("The sum is: ");
+            Console.WriteLine(nums.Sum());
+        }
+    }
+}
