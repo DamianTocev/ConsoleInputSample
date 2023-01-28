f617b801acf12e5d010642dc4ceba291a47b0426
Damian Tocev <damian.tocev@gmail.com>
2023-01-28 23:40:52 +0200

    Add new tests

--- ProgramUnitTests/ProgramTests.cs
+++ ProgramUnitTests/ProgramTests.cs
@@ -5,7 +5,7 @@ namespace ProgramUnitTests
     public class ProgramTests
     {
         [Test]
-        public void Test_ProgramSum_TwoPositiveNumbers()
+        public void Test_ProgramSum_ThreePositiveNumbers()
         {
             // Arrange
             string input = "5 10 12";
@@ -20,8 +20,91 @@ namespace ProgramUnitTests
 
             // Assert
             var actualResult = writer.ToString();
-            var expectedResult = "The sum is: " + Environment.NewLine +
-                "27" + Environment.NewLine;
+            var expectedResult = "The sum is: " + Environment.NewLine + "27" + Environment.NewLine;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Test_ProgramSum_TwoPositiveNumbers()
+        {
+            // Arrange
+            string input = "10 12";
+            var writer = new StringWriter();
+            Console.SetOut(writer); // Redirect Console.Out
+
+            var reader = new StringReader(input);
+            Console.SetIn(reader); // Redirect Console.In
+
+            // Act
+            ConsoleInputSample.ProgramTest.Main();
+
+            // Assert
+            var actualResult = writer.ToString();
+            var expectedResult = "The sum is: " + Environment.NewLine + "22" + Environment.NewLine;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Test_ProgramSum_OnePositiveNumbers()
+        {
+            // Arrange
+            string input = "5";
+            var writer = new StringWriter();
+            Console.SetOut(writer); // Redirect Console.Out
+
+            var reader = new StringReader(input);
+            Console.SetIn(reader); // Redirect Console.In
+
+            // Act
+            ConsoleInputSample.ProgramTest.Main();
+
+            // Assert
+            var actualResult = writer.ToString();
+            var expectedResult = "The sum is: " + Environment.NewLine + "5" + Environment.NewLine;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Test_ProgramSum_ThreeNegativeNumbers()
+        {
+            // Arrange
+            string input = "-5 -10 -12";
+            var writer = new StringWriter();
+            Console.SetOut(writer); // Redirect Console.Out
+
+            var reader = new StringReader(input);
+            Console.SetIn(reader); // Redirect Console.In
+
+            // Act
+            ConsoleInputSample.ProgramTest.Main();
+
+            // Assert
+            var actualResult = writer.ToString();
+            var expectedResult = "The sum is: " + Environment.NewLine + "-27" + Environment.NewLine;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Test_ProgramSum_OnePositive_And_TwoNegative_Numbers()
+        {
+            // Arrange
+            string input = "5 -10 -12";
+            var writer = new StringWriter();
+            Console.SetOut(writer); // Redirect Console.Out
+
+            var reader = new StringReader(input);
+            Console.SetIn(reader); // Redirect Console.In
+
+            // Act
+            ConsoleInputSample.ProgramTest.Main();
+
+            // Assert
+            var actualResult = writer.ToString();
+            var expectedResult = "The sum is: " + Environment.NewLine + "-17" + Environment.NewLine;
 
             Assert.AreEqual(expectedResult, actualResult);
         }
